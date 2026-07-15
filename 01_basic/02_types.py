678e4dd1696a1afcca06a9d37da0b4cb8bcf46ab
Jaime <jaimelopezmislata@gmail.com>
2026-07-15 13:03:56 +0200

    Tipos de datos y conversiones

---
+++ 01_basic/02_types.py
@@ -0,0 +1,22 @@
+print("Mostrando tipos de datos:")
+
+print("int:")
+print(7)
+print(type(1))
+print(type(-2))
+
+print("str:")
+print("Hello, World!")
+
+print("float:")
+print(4.5)
+print(type(1.0))
+print(type(-2.5))
+
+print("bool:")
+print(1 < 2)
+print(type(True))
+print(type(False))
+
+print("None:")
+print(None)
---
+++ 01_basic/03_casting.py
@@ -0,0 +1,5 @@
+print("Conversión de tipos de datos:")
+print(int("100") + 7)
+print("100" + str(7))
+
+print(int(7.9))
